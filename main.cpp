c91043c62b7c677f937ccbabe3b5f40f5995ec68
Mike Kinney <mike.kinney@gmail.com>
2022-01-17 13:57:50 -0800

    change spacing

--- main.cpp
+++ main.cpp
@@ -5,14 +5,14 @@
 #include "messages.hpp"
 
 int main() {
-	std::string message_string = get_message_string();
-	std::cout << "message_string:" << message_string << std::endl;
-	std::string messages[MAX_CANNED_MESSAGES];
-	split_messages(message_string, messages);
-	std::cout << "messages:" << std::endl;
-	for (int i = 0; i < MAX_CANNED_MESSAGES; i++) {
-		if (messages[i] != "") {
-			std::cout << " " << messages[i] << std::endl;
-		}
-	}
+    std::string message_string = get_message_string();
+    std::cout << "message_string:" << message_string << std::endl;
+    std::string messages[MAX_CANNED_MESSAGES];
+    split_messages(message_string, messages);
+    std::cout << "messages:" << std::endl;
+    for (int i = 0; i < MAX_CANNED_MESSAGES; i++) {
+        if (messages[i] != "") {
+            std::cout << " " << messages[i] << std::endl;
+        }
+    }
 }
--- test_messages.cpp
+++ test_messages.cpp
@@ -2,37 +2,37 @@
 #include "messages.hpp"
 
 void test_get_message_string(void) {
-	std::string message_string = get_message_string();
-	TEST_CHECK(message_string == "This is one|This is two|This is three|This is four|This is five|");
+    std::string message_string = get_message_string();
+    TEST_CHECK(message_string == "This is one|This is two|This is three|This is four|This is five|");
 }
 
 void test_split_messages_empty(void) {
-	std::string messages[MAX_CANNED_MESSAGES];
-	split_messages("", messages);
-	TEST_CHECK(messages_count(messages) == 0);
+    std::string messages[MAX_CANNED_MESSAGES];
+    split_messages("", messages);
+    TEST_CHECK(messages_count(messages) == 0);
 }
 
 void test_split_messages_default(void) {
-	std::string messages[MAX_CANNED_MESSAGES];
-	std::string message_string = get_message_string();
-	split_messages(message_string, messages);
-	TEST_CHECK(messages_count(messages) == 5);
+    std::string messages[MAX_CANNED_MESSAGES];
+    std::string message_string = get_message_string();
+    split_messages(message_string, messages);
+    TEST_CHECK(messages_count(messages) == 5);
 }
 
 void test_init_messages(void) {
-	std::string messages[MAX_CANNED_MESSAGES];
-	TEST_CHECK(messages_count(messages) == 0);
-	std::string message_string = get_message_string();
-	split_messages(message_string, messages);
-	TEST_CHECK(messages_count(messages) == 5);
-	init_messages(messages);
-	TEST_CHECK(messages_count(messages) == 0);
+    std::string messages[MAX_CANNED_MESSAGES];
+    TEST_CHECK(messages_count(messages) == 0);
+    std::string message_string = get_message_string();
+    split_messages(message_string, messages);
+    TEST_CHECK(messages_count(messages) == 5);
+    init_messages(messages);
+    TEST_CHECK(messages_count(messages) == 0);
 }
 
 TEST_LIST = {
-   { "get_message_string", test_get_message_string},
-   { "split_messages_empty", test_split_messages_empty},
-   { "split_messages_default", test_split_messages_default},
-   { "init_messages", test_init_messages},
-   { NULL, NULL }     /* zeroed record marking the end of the list */
+    { "get_message_string", test_get_message_string},
+    { "split_messages_empty", test_split_messages_empty},
+    { "split_messages_default", test_split_messages_default},
+    { "init_messages", test_init_messages},
+    { NULL, NULL }     /* zeroed record marking the end of the list */
 };
